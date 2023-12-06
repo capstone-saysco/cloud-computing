8c03b38f616a27aeb232bf59b4dd9e31d2ff571d
Alya Zakhira Anjani <zakhiralya@gmail.com>
2023-12-06 16:18:32 +0700

    added essay endpoint

---
+++ controllers/essayController.js
@@ -0,0 +1,262 @@
+import { connection } from "../config/database.js";
+
+// [GET] Read all essay pack based on user authorized
+export const getAllEssayPacks = async(req, res) => {
+    try {
+        const user_id = res.locals.user_id;
+        connection.query('SELECT * FROM `essay-pack` WHERE user_id='+user_id,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(404).json({message: 'This user has not created an essay pack.'});
+                } else {
+                    res.status(200).json({
+                        message: 'All essay pack data.',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [GET] Read all essay based on pack ID
+export const getAllEssays = async(req, res) => {
+    try {
+        const pack_id = req.params.pack_id;
+        connection.query('SELECT * FROM essay WHERE pack_id='+pack_id, 
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(404).json({message: 'There are no pack with id '+pack_id});
+                } else {
+                    res.status(200).json({
+                        message: 'All essays data on the pack.',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [GET] Show an essay pack based on its ID
+export const showEssayPack = async(req, res) => {
+    try {
+        const pack_id = req.params.pack_id;
+        conn.query('SELECT * FROM essay-pack WHERE id='+pack_id, 
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(404).json({message: 'There are no essay pack with id '+pack_id});
+                } else {
+                    res.status(200).json({
+                        message: 'Showed essay pack data.',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+
+}
+
+// [GET] Show an essay based on its ID
+export const showEssay = async(req, res) => {
+    try {
+        const essay_id = req.params.essay_id;
+        let essayData;
+        let packData;
+        connection.query('SELECT * FROM essay WHERE id='+essay_id, 
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(404).json({message: 'There are no essay with id '+essay_id});
+                } else {
+                    essayData = rows[0];
+                }
+            }
+        );
+
+        connection.query('SELECT * FROM essay-pack WHERE id='+essayData.pack_id,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(404).json({message: 'There are no essay pack with id '+essayData.pack_id});
+                } else {
+                    packData = rows[0];
+                }
+            }
+        )
+
+        res.status(200).json({
+            message: 'Showed essay data.',
+            data: {
+                id: essay_id,
+                pack_title: packData.title,
+                student_name: essayData.student_name,
+                student_number: essayData.student_number,
+                question: packData.question,
+                answer: essayData.answer,
+                score: essayData.score,
+            }
+        });
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [POST] Add an essay pack
+export const addEssayPack = async(req, res) => {
+    try {
+        const user_id = res.locals.user_id;
+        const {title, question} = req.body;
+        const addData = {
+            user_id: user_id,
+            title: title,
+            question: question
+        }
+
+        connection.query('INSERT INTO `essay-pack` SET ?', addData,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(201).json({
+                        message: 'Essay pack added successfully.',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [POST] Add an essay on an essay pack
+export const addEssay = async(req, res) => {
+    try {
+        const pack_id = req.params.pack_id;
+        const {student_name, student_number, answer, score} = req.body;
+        const addData = {
+            pack_id: pack_id,
+            student_name: student_name,
+            student_number: student_number,
+            answer: answer,
+            score: score
+        }
+
+        connection.query('INSERT INTO `essay` SET ?', addData,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(201).json({
+                        message: 'Essay added successfully.',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [PUT] Update an essay pack
+export const updateEssayPack = async(req, res) => {
+    try {
+        const pack_id = req.params.pack_id;
+        const {title, question} = req.body;
+        const updateData = {
+            title: title,
+            question: question
+        }
+
+        connection.query('UPDATE `essay-pack` SET ? WHERE id='+pack_id, updateData,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(200).json({
+                        message: 'Essay pack updated successfully!',
+                        data: rows
+                    });
+                }
+            }
+        )
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [PUT] Update an essay
+export const updateEssay = async(req, res) => {
+    try {
+        const essay_id = req.params.essay_id;
+        const {student_name, student_number, answer, score} = req.body;
+        const updateData = {
+            student_name: student_name,
+            student_number: student_number,
+            answer: answer,
+            score: score
+        }
+
+        connection.query('UPDATE `essay` SET ? WHERE id='+essay_id, updateData,
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(200).json({
+                        message: 'Essay updated successfully!',
+                        data: rows
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [DELETE] Delete a pack with its entire content
+export const deleteEssayPack = async(req, res) => {
+    try {
+        const pack_id = req.params.pack_id;
+        conn.query("DELETE FROM `essay-pack` WHERE id="+pack_id,  
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(200).json({
+                        message: 'Essay pack and its content deleted successfully.'
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
+
+// [DELETE] an essay
+export const deleteEssay = async(req, res) => {
+    try {
+        const essay_id = req.params.essay_id;
+        conn.query("DELETE FROM `essay` WHERE id="+essay_id,  
+            (err, rows, fields) => {
+                if (err) {
+                    res.status(500).json({message: err});
+                } else {
+                    res.status(200).json({
+                        message: 'Essay deleted successfully.'
+                    });
+                }
+            }
+        );
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import { register, login, logout, refreshToken } from "../controllers/authController.js";
 import { showUser } from "../controllers/userController.js"
 import { verifyToken } from "../middleware/auth.js";
+import { addEssay, addEssayPack, deleteEssay, deleteEssayPack, getAllEssayPacks, getAllEssays, showEssay, showEssayPack, updateEssay, updateEssayPack } from "../controllers/essayController.js";
 const router = express.Router();
 
 // Auth routes
@@ -14,5 +15,15 @@ router.delete('/logout', verifyToken, logout);
 router.get('/user', verifyToken, showUser);
 
 // Essay routes
+router.get('/essay', verifyToken, getAllEssayPacks);
+router.get('/essay/item', verifyToken, getAllEssays);
+router.get('/essay/:pack_id', verifyToken, showEssayPack);
+router.get('/essay/item/:essay_id', verifyToken, showEssay);
+router.post('/essay', verifyToken, addEssayPack);
+router.post('/essay/item/:pack_id', verifyToken, addEssay);
+router.put('/essay/:pack_id', verifyToken, updateEssayPack);
+router.put('/essay/item/:essay_id', verifyToken, updateEssay);
+router.delete('/essay/:pack_id', verifyToken, deleteEssayPack);
+router.delete('/essay/item/:essay_id', verifyToken, deleteEssay);
 
 export default router;
